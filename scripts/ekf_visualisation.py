053ace90a49ba5bd96e366d74e423aceffc09510
Sun Yitao <sunyitao7@gmail.com>
2021-07-31 20:41:18 +0800

    u

--- scripts/ekf_visualisation.py
+++ scripts/ekf_visualisation.py
@@ -2,13 +2,49 @@
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose)
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+odom, imu, combined = [], [], []
+
+def callback_combined(data):
+    if len(combined) < 50:
+        combined.append(data.pose.position)
+    else:
+        x,y,types = [],[],[]
+        for p in odom:
+            x.append(p.x)
+            y.append(p.y)
+            types.append('odom')
+        for p in imu:
+            x.append(p.x)
+            y.append(p.y)
+            types.append('imu')
+        for p in combined:
+            x.append(p.x)
+            y.append(p.y)
+            types.append('combined')
+        df = pd.DataFrame(data={'x': x, 'y': y, 'type': types})
+        plot = sns.scatterplot(x="x", y="y", hue='type', data=df)
+        plot.figure.savefig("/home/parallels/Downloads/output.png")
+        rospy.loginfo("Figure saved")
+
+
+
+def callback_odom(data):
+    odom.append(data.pose.position)
+
+def callback_imu(data):
+    imu.append(data.pose.position)
     
 def listener():
     rospy.init_node('ekf_visualisation', anonymous=True)
 
-    rospy.Subscriber("/robot_pose_ekf/odom_combined", PoseWithCovarianceStamped, callback)
+    rospy.Subscriber("/robot_pose_ekf/odom_combined", PoseWithCovarianceStamped, callback_combined)
+    rospy.Subscriber("odom", PoseWithCovarianceStamped, callback_odom)
+    rospy.Subscriber("/imu", PoseWithCovarianceStamped, callback_imu)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
