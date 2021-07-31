773923d1a892b9fd9849d11773f4befb4bf8a721
Sun Yitao <sunyitao7@gmail.com>
2021-07-31 17:14:08 +0800

    U

---
+++ scripts/ekf_visualisation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from geometric_msgs.msg import PoseWithCovarianceStamped
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+def listener():
+    rospy.init_node('ekf_visualisation', anonymous=True)
+
+    rospy.Subscriber("/robot_pose_ekf/odom_combined", PoseWithCovarianceStamped, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
